74b2b57034c1ee97e674a95ab2b76ca703557cee
Phenomenalhub <96958674+Phenomenalhub@users.noreply.github.com>
2023-03-12 18:36:42 -0500

    Added a collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Student on 3/12/23.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
